0445fa27d6ee530452d5c0e1fd0275a90dc2c929
edclements <ed.clements@gmail.com>
2016-11-16 12:04:30 +0000

    Version v2.1.0-beta.6

--- bookingbug-angular-events.js
+++ bookingbug-angular-events.js
@@ -310,27 +310,3 @@
   });
 
 }).call(this);
-
-(function() {
-  'use strict';
-  angular.module('BBAdminEvents').config(function($translateProvider) {
-    'ngInject';
-    var translations;
-    translations = {
-      EVENTS: {
-        EVENT_CHAIN_TABLE: {
-          NEW_EVENT_CHAIN_BTN: 'New Event Chain',
-          DELETE_BTN: '@:COMMON.BTN.DELETE',
-          EDIT_BTN: '@:COMMON.BTN.EDIT'
-        },
-        EVENT_GROUP_TABLE: {
-          NEW_EVENT_GROUP: 'New Event Group',
-          DELETE_BTN: '@:COMMON.BTN.DELETE',
-          EDIT_BTN: '@:COMMON.BTN.EDIT'
-        }
-      }
-    };
-    $translateProvider.translations('en', translations);
-  });
-
-}).call(this);
